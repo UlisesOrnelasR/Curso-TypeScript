5994a635c257350071f1c57a44229f75da716a44
UlisesFOrnelas <ornelas.ulises19@gmail.com>
2023-01-14 22:33:28 -0700

    Chore: configuring typescript environment

---
+++ hola-mundo/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let msj = "Hola mundo";
+msj = "chanchito feliz";
+console.log(msj);
+msj = "chaoo mundo";
+//# sourceMappingURL=index.js.map
---
+++ hola-mundo/src/index.ts
@@ -0,0 +1,7 @@
+let msj: string = "Hola mundo"
+
+msj = "chanchito feliz"
+
+console.log(msj)
+
+msj = "chaoo mundo"
