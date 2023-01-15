e2f1fbe8937837902b0bf2bdd6339b03c1d15da6
UlisesFOrnelas <ornelas.ulises19@gmail.com>
2023-01-15 11:10:36 -0700

    Chore: Theoretical part of the language was learned

--- hola-mundo/src/index.ts
+++ hola-mundo/src/index.ts
@@ -5,3 +5,13 @@ msj = "chanchito feliz"
 console.log(msj)
 
 msj = "chaoo mundo"
+
+
+let extincionDinosaurios: number = 76_000_000
+let dinosaurioFavorito: string = "T-rex"
+let extintos = true
+
+let miVariable 
+miVariable = "chanchito"
+miVariable = 42
+
